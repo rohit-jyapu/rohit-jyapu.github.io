adc7babf1ef1bd48acec09e8fd8bdf6da63c28b4
rohit-jyapu <rohit.jyapu@gmail.com>
2017-06-04 09:31:03 +0545

    third project deleted

--- bhuwaneshwor/includes/head.php
+++
@@ -1,5 +0,0 @@
-<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-
-	<link rel="stylesheet" type="text/css" href="slick/slick/slick.css">
-  <link rel="stylesheet" type="text/css" href="slick/slick/slick-theme.css">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
